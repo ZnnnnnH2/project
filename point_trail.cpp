7ae1a496096534ccc315bf3b584d072869abfefc
Zhang Nianhao <zhangnianhao@ruc.edu.cn>
2024-12-12 11:08:16 +0800

    malloc

--- point_trail.cpp
+++ point_trail.cpp
@@ -66,5 +66,9 @@ int main()
     for (int i = 0; i < 10;i++){
         arr[i] = (intt)malloc(10 * sizeof(int));
     }
+    for (int i = 0; i < 10;i++){
+        free(arr[i]);
+    }
+    free(arr);
         return 0;
 }
